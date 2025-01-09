9fcba295020924ee04f3727bcd928d20143b5b23
a3ylf <alexandre3ylf@gmail.com>
2025-01-09 10:21:33 -0300

    Base logic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module plight
+
+go 1.23.4
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+    //Format to save on file
+	a := time.Now().Format(time.DateTime)
+	fmt.Println(a)
+	time.Sleep(3 * time.Second)
+	b := time.Now().Format(time.DateTime)
+	fmt.Println(b)
+
+    //Retrieve from file
+	fmt.Println()
+	c , _ := time.Parse(time.DateTime,a)
+    d , _ := time.Parse(time.DateTime,b)
+    e := d.Sub(c).String()
+	fmt.Println(e)
+    x, _ := time.ParseDuration(e)
+    fmt.Println(x)
+}
