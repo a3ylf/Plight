fff867e52c3e5993cd5a8d919d0cf6b64becea94
apns <alexandre3ylf@gmail.com>
2025-01-22 17:55:18 -0300

    Raw flag

--- flags/flags.go
+++ flags/flags.go
@@ -5,11 +5,13 @@ import "flag"
 var (
     Reset bool
     Dev bool
+    Raw bool
 )
 
 func ParseFlags() {
     flag.BoolVar(&Dev,"dev", false,"Use file debug.json")
     flag.BoolVar(&Reset,"reset", false,"reset file")
+    flag.BoolVar(&Raw,"raw", false,"receive raw data")
     flag.Parse()
 }
 func ParseArgs() []string {
