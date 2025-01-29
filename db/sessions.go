ad78a3f575745c3b829a2516fbea33ba303801e9
apns <alexandre3ylf@gmail.com>
2025-01-29 18:37:36 -0300

    Supercharging deluxe sessionizer 39.3

--- db/sessions.go
+++ db/sessions.go
@@ -23,15 +23,14 @@ func (p *Plight) GetSession(session string) ([]byte, error) {
 	return send, nil
 }
 
-func (p *Plight) GetSessions() ([]byte, error) {
+func (p *Plight) GetData() (*Data, error) {
 
 	data, err := p.ReadDB()
 
 	if err != nil {
-		return []byte{}, err
+		return &Data{}, err
 	}
-	d, err := json.MarshalIndent(data.Sessions, "", "   ")
-	return d, err
+	return data, err
 }
 func (p *Plight) SessionAdd(session string) error {
 
@@ -40,7 +39,8 @@ func (p *Plight) SessionAdd(session string) error {
 	if err != nil {
 		return err
 	}
-	timenow := fmt.Sprint(time.Now().Date())
+	daynow := fmt.Sprint(time.Now().Date())
+	timenow := time.Now().Format(time.TimeOnly)
 
 	if data.Sessions == nil {
 		data.Sessions = make(map[string]Days)
@@ -48,44 +48,48 @@ func (p *Plight) SessionAdd(session string) error {
 	if _, e := data.Sessions[session]; !e {
 		data.Sessions[session] = Days{}
 	}
-	last := len(data.Sessions[session][timenow].Periods) - 1
+	last := len(data.Sessions[session][daynow].Periods) - 1
 
 	if last == -1 {
-		a := Day{
+		day := Day{
 			Day_Total: "0s",
 			Periods: []Period{
 				{
-					From: time.Now().Format(time.TimeOnly)},
+					From: timenow,
+				},
 			},
 		}
-		data.Sessions[session][timenow] = a
-
-	} else if data.Sessions[session][timenow].Periods[last].To == "" {
-		now := time.Now().Format(time.TimeOnly)
-		data.Sessions[session][timenow].Periods[last].To = now
-		dur, err := time.ParseDuration(data.Sessions[session][timenow].Day_Total)
-		from, err := time.Parse(time.TimeOnly, data.Sessions[session][timenow].Periods[last].From)
-		to, err := time.Parse(time.TimeOnly, data.Sessions[session][timenow].Periods[last].To)
+		data.Sessions[session][daynow] = day
+		fmt.Printf("Time added to %v, %v\n", session, timenow)
+		// forgive me for this
+	} else if data.Sessions[session][daynow].Periods[last].To == "" {
+		data.Sessions[session][daynow].Periods[last].To = timenow
+		dur, err := time.ParseDuration(data.Sessions[session][daynow].Day_Total)
+		from, err := time.Parse(time.TimeOnly, data.Sessions[session][daynow].Periods[last].From)
+		to, err := time.Parse(time.TimeOnly, data.Sessions[session][daynow].Periods[last].To)
 
 		if err != nil {
 			return err
 		}
+		dursess := to.Sub(from)
 		newTotal := to.
 			Add(dur).
 			Sub(from).
 			String()
 
-		s := data.Sessions[session][timenow]
+		s := data.Sessions[session][daynow]
 		s.Day_Total = newTotal
 
-		data.Sessions[session][timenow] = s
+		data.Sessions[session][daynow] = s
+        fmt.Printf("Session ended\nSession duration: %v\nNew total duration: %v\n", dursess, newTotal)
 
 	} else {
-		a := data.Sessions[session][timenow]
+		a := data.Sessions[session][daynow]
 		a.Periods = append(a.Periods, Period{
 			Id:   last + 1,
-			From: time.Now().Format(time.TimeOnly)})
-		data.Sessions[session][timenow] = a
+			From: timenow,})
+		data.Sessions[session][daynow] = a
+		fmt.Printf("Time added to %v, %v\n", session, timenow)
 	}
 
 	err = p.writeDB(data)
