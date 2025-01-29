bd028ebcd84870edef3ec696d137ee49388c1c34
apns <alexandre3ylf@gmail.com>
2025-01-29 19:03:47 -0300

    confirm before hitting

--- db/hits.go
+++ db/hits.go
@@ -1,6 +1,10 @@
 package db
 
-import "time"
+import (
+	"errors"
+	"fmt"
+	"time"
+)
 
 func (p *Plight) HitAdd(hit string) error {
 	data, err := p.ReadDB()
@@ -11,6 +15,12 @@ func (p *Plight) HitAdd(hit string) error {
 	today := time.Now().Format(time.DateOnly)
 
 	if _, e := data.Hits[hit]; !e {
+        fmt.Printf("You are creating a new hit, rewrite it's name to confirm\n%v: ",hit)
+        var check string
+        fmt.Scan(&check)
+        if check != hit {
+            return (errors.New("hit not created"))
+        }
 		data.Hits[hit] = make(map[string]int)
 
 	}
